0fc1897938b018cbdf8ba84da2f329ef61bbc43a
defirmansyh <firmansyahdede4@gmail.com>
2021-01-20 10:29:21 +0700

    End of section 6

---
+++ client/src/app/members/membe-detail/membe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-membe-detail',
+  templateUrl: './membe-detail.component.html',
+  styleUrls: ['./membe-detail.component.css']
+})
+export class MembeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
